eb4c1ff996121075a2bf50f9d0bcb0c2b9e9edfb
Fridapeersson <108325710+Fridapeersson@users.noreply.github.com>
2023-03-25 22:39:37 +0100

    lagt till kommentarer

--- client/src/main/java/com/example/demo/ClientApplication.java
+++ client/src/main/java/com/example/demo/ClientApplication.java
@@ -16,6 +16,7 @@ public class ClientApplication {
 		
 		Scanner scan = new Scanner(System.in);
 		
+		//Loop som fortsätter tills användaren skriver quit
 		while(true) {
 			System.out.println("Skriv in ditt val");
 			System.out.println("1. Hämta information om all användare");
@@ -39,15 +40,15 @@ public class ClientApplication {
 			String urlMod = "null";
 			
 			if (userChoice == 1 || userChoice == 2) {
+				//Om användaren väljer 1 ändras urlMod till users
 				if(userChoice == 1) {
-
 					urlMod = "users";
 				} else if (userChoice == 2) {
 					System.out.println("Ange id nummer på användare (1-6): ");
-
+					//läser in id nummer från användaren
 					int chosenId = scan.nextInt();
 					scan.nextLine();
-
+					//Är id´t giltligt ändras urlMod till user=id= + chosenId
 					if(chosenId >= 1 && chosenId <= 6) {
 						urlMod = "user?id=" + chosenId;
 					} else {
@@ -60,12 +61,14 @@ public class ClientApplication {
 				continue;
 			}
 			
-
+			//skapar http get request med den angivna url:en
 			HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url + urlMod)).GET().build();
 			
+			//Skapar http klient och skickar requesten och sparar svaret i en httpResponse
 			HttpClient client = HttpClient.newHttpClient();
 			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 			
+			//Skriver ut svaret från servern
 			System.out.println(response.body());
 			System.out.println("------------------------------------------------------------------------");
 			
